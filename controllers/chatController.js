3dc84611aa7219539fd2051d25f78ab2d409b8e5
THKHIEM2310 <khiewcokk@gmail.com>
2026-04-10 10:59:18 +0700

    fix loi model gemini

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -47,7 +47,7 @@ async function handleChat(req, res) {
         CÂU HỎI CỦA NGƯỜI DÙNG: ${userMessage}`;
 
         const response = await ai.models.generateContent({
-            model: "gemini-2.5-flash", 
+            model: "gemini-3-flash-preview", 
             contents: prompt,
         });
 
